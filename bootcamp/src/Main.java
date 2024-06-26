82f12ca83e080be3c4f455b566ba6ee5eb8f3a5e
egroehs <eduardagroehs@gmail.com>
2024-06-26 11:07:29 -0700

    Created new curso and mentoria

--- bootcamp/src/Main.java
+++ bootcamp/src/Main.java
@@ -1,9 +1,27 @@
+import java.time.LocalDate;
+
 import main.java.br.com.dio.desafio.dominio.Curso;
+import main.java.br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descricao");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Java 2");
+        curso2.setDescricao("Descricao 2");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Mentoria Java");
+        mentoria.setData(LocalDate.now());
 
         System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
